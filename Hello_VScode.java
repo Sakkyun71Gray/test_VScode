7db14820c29bb85ec67cda90482c1ed4dfc4e3eb
Sakkyun71Gray <85877280+Sakkyun71Gray@users.noreply.github.com>
2022-04-08 11:34:03 +0900

    ローカルディレクトリの変更

--- Hello_VScode.java
+++ Hello_VScode.java
@@ -2,5 +2,6 @@ public class Hello_VScode {
     public static void main(String[] args) {
         System.out.println("Hello, VScode!");
         System.out.println("Hello, GitHub!");
+        System.out.println("Change directory");
     }
 }
