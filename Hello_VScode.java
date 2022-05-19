3342eda6d5f4f88d5a4c13f42665b369f176397a
{Sakkyun71Gray} <{yuugenzikkkou0718@gmail.com}>
2022-05-19 11:24:10 +0900

    別のpcからのコミット

--- Hello_VScode.java
+++ Hello_VScode.java
@@ -5,5 +5,6 @@ public class Hello_VScode {
         System.out.println("Hello, GitHub!");
         System.out.println("Change directory");
         System.out.println("relogin the account");
+        System.out.println("sign in for another PC");
     }
 }
